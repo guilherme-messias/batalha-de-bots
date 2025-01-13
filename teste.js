8d27a0c6c5e27c72bbacbb326fe3cdf957cf3525
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-13 05:21:18 -0300

    adiciona lógica para loop de ataques entre robôs e determina o vencedor

--- teste.js
+++ teste.js
@@ -1,13 +1,23 @@
 function name(b1, b2, result) {
+  let bot1, bot2;
   if (b1.name === result) {
-    const bot1 = b1;
-    const bot2 = b2;
+    bot1 = b1;
+    bot2 = b2;
   } else {
-    const bot1 = b2;
-    const bot2 = b1;
+    bot1 = b2;
+    bot2 = b1;
   }
 
-  // todo: criar loop dos ataques
+  // energia de ambos precisa ser superior a 0
+  while (bot1.energy >= 0 && bot2.energy >= 0) {
+    // bot1 ataca bot2
+    bot2.energy -= bot1.attack;
+    if (bot2.energy <= 0) break;
 
+    // bot2 ataca bot1
+    bot1.energy -= bot2.attack;
+  }
 
+  // Retorna o vencedor
+  return bot1.energy > 0 ? bot1 : bot2;
 }
