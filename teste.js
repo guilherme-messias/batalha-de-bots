779c72a7bac4e68671f2484a69bf3825a855c4db
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-13 05:03:49 -0300

    adiciona função para determinar o bot vencedor e prepara estrutura para loop de ataques

---
+++ teste.js
@@ -0,0 +1,13 @@
+function name(b1, b2, result) {
+  if (b1.name === result) {
+    const bot1 = b1;
+    const bot2 = b2;
+  } else {
+    const bot1 = b2;
+    const bot2 = b1;
+  }
+
+  // todo: criar loop dos ataques
+
+
+}
